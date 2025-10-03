1dd84935305ba537068d57fe79ab2c9f0c8ffd0f
codemmk <2107228359@qq.com>
2025-10-03 18:13:37 +0800

    feat: Request encapsulation

---
+++ app/utils/useRequest.ts
@@ -0,0 +1,55 @@
+export const useRequest = async (url: string, options?: any) => {
+  try {
+    const reqUrl = useRuntimeConfig().public.api_url + url
+
+    // 可以设置默认headers例如
+    const customHeaders = {
+      Authorization: useCookie('token').value
+    }
+
+    let body
+
+    const { data } = await useFetch(reqUrl, {
+      ...options,
+      headers: customHeaders,
+      watch: false,
+
+      // onResponseError({ response }) {
+      //   switch (response.status) {
+      //     case 400:
+      //       message.error(response._data.message)
+      //       body = response._data
+      //       return
+      //     case 401:
+      //       message.error(response._data.message)
+      //       useCookie('token').value = null
+      //       useMainStore().setHaslogin(false)
+      //       navigateTo('/login')
+      //       body = response._data
+      //       return
+      //     case 403:
+      //       message.error('服务器拒绝访问')
+      //       body = response._data
+      //       return
+      //     case 404:
+      //       message.error('请求地址错误')
+      //       body = response._data
+      //       return
+      //     case 500:
+      //       message.error('服务器故障')
+      //       body = response._data
+      //       return
+      //     default:
+      //       message.error('网络连接故障')
+      //       body = response._data
+      //   }
+      // }
+    })
+
+    if(!data.value) return body
+
+    return data.value
+  } catch (err) {
+    return Promise.reject(err)
+  }
+}
